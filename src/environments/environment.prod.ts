66cb8ecf1da4cf1799840a43a224b4acd97a8704
Matias Bernardi <matias.bernardi.1@gmail.com>
2024-02-28 11:22:41 -0300

    environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.example.com', // URL para producción
+  apiUrl: 'https://comuna-backend.onrender.com/api', // URL para producción
 };
